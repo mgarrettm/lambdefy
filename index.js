6e48beae82860e883db766d143ae196d6e9f3902
Garrett McGrath <garrett.mcgrath@gmail.com>
2016-02-19 16:50:18 -0500

    Implement acsync support that actually works.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var querystring = require('querystring');
+var http;
 
 var responseHeaders;
 
@@ -8,76 +9,83 @@ module.exports = function (port, options) {
 
     if (!options) options = {};
 
-    var http = options.https ? require('https') : require('http');
-    var async = options.async;
+    http = options.https ? require('https') : require('http');
 
-    var async_event = null;
-    var async_context = null;
+    var async = options.async;
+    var ready = false;
+    var queue = [];
 
     return function (event, context) {
-        
-        if (async) {
+
+        if (async && !ready) {
             if (event) {
-                async_event = event;
-                async_context = context;
-                return;
-            } else if (async_event) {
-                event = async_event;
-                context = async_context;
+                queue.push({
+                    'event': event,
+                    'context': context
+                });
             } else {
-                return;
+                ready = true;
+                queue.forEach(function (item) {
+                    handle(port, item.event, item.context);
+                });
             }
+            return;
         }
 
-        if (!responseHeaders) responseHeaders = event.responseHeaders;
-
-        var parts = [];
-        for (var p = 0; 'p' + p.toString() in event.path; p++) {
-            parts.push(event.path['p' + p.toString()]);
-        }
-        event.path = '/' + parts.join('/');
-
-        event.query = querystring.stringify(event.query);
-        if (event.query) {
-            event.path += '?' + event.query;
-        }
-
-        var options = {
-            hostname: 'localhost',
-            port: port,
-            method: event.method,
-            path: event.path,
-            headers: event.headers
-        };
-
-        console.log(options);
-
-        var req = http.request(options, function (res) {
-
-            res.setEncoding('utf8');
-
-            var body = '';
-            res.on('data', function (chunk) {
-                body += chunk;
-            });
-
-            res.on('end', function () {
-                context.done(createError(res.statusCode, body, res.headers));
-            });
-        });
-
-        req.on('error', function (error) {
-            context.done(createError(500, JSON.stringify({
-                message: error.message
-            })));
-        });
-
-        if (event.method.substring(0, 1) === 'P') {
-            req.write(event.body);
-        }
-
-        req.end();
+        handle(port, event, context);
     }
+}
+
+function handle(port, event, context) {
+
+    if (!responseHeaders) responseHeaders = event.responseHeaders;
+
+    var parts = [];
+    for (var p = 0; 'p' + p.toString() in event.path; p++) {
+        parts.push(event.path['p' + p.toString()]);
+    }
+    event.path = '/' + parts.join('/');
+
+    event.query = querystring.stringify(event.query);
+    if (event.query) {
+        event.path += '?' + event.query;
+    }
+
+    var options = {
+        hostname: 'localhost',
+        port: port,
+        method: event.method,
+        path: event.path,
+        headers: event.headers
+    };
+
+    console.log(options);
+
+    var req = http.request(options, function (res) {
+
+        res.setEncoding('utf8');
+
+        var body = '';
+        res.on('data', function (chunk) {
+            body += chunk;
+        });
+
+        res.on('end', function () {
+            context.done(createError(res.statusCode, body, res.headers));
+        });
+    });
+
+    req.on('error', function (error) {
+        context.done(createError(500, JSON.stringify({
+            message: error.message
+        })));
+    });
+
+    if (event.method.substring(0, 1) === 'P') {
+        req.write(event.body);
+    }
+
+    req.end();
 }
 
 function createError(statusCode, body, headers) {
